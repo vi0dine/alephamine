94334059444bcc541bc061392d9553b6130f7dc3
viodine <viodine@yahoo.com>
2021-01-31 17:13:18 +0100

    Cron fix

--- backend/config/schedule.rb
+++ backend/config/schedule.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 
-set :output, 'log/cron_log.log'
+set :output, 'log/cron.log'
 set :environment, 'production'
+
+ENV.each { |k, v| env(k, v) }
 
 every 20.minutes do
   rake 'spiders:crawl'
